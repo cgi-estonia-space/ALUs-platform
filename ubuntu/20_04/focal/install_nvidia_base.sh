4f2ec5f7a3f8c6e8d2b79e4daaa28d2a053b8537
Sven Kautlenbach <sven.kautlenbach@cgi.com>
2022-12-30 15:35:45 +0200

    Tested and fixed Ubuntu 20.04 dependency installation scripts.

--- ubuntu/20_04/focal/install_nvidia_base.sh
+++ ubuntu/20_04/focal/install_nvidia_base.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -e
+
 apt update
-apt install -y gcc g++
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+apt install -y gcc g++ software-properties-common
+# From https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=deb_network
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+dpkg -i cuda-keyring_1.0-1_all.deb
 apt update
-apt -y install cuda-11-2
-echo "" >> .bashrc
-echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64/:$LD_LIBRARY_PATH" >> ~/.bashrc
-echo "export PATH=/usr/local/cuda/bin/:$PATH" >> ~/.bashrc
+apt install -y cuda-compiler-11-2 cuda-libraries-dev-11-2
+echo "" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.2/lib64/:$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export PATH=/usr/local/cuda-11.2/bin/:$PATH" >> ~/.bashrc
+
--- ubuntu/20_04/focal/setup_dev.sh
+++ ubuntu/20_04/focal/setup_dev.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-apt update && curl https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null |  apt-key add - &&  apt-add-repository -y 'deb https://apt.kitware.com/ubuntu/ focal main' &&  apt update &&  apt install -y cmake --no-install-recommends``
-apt install clang clang-format-10 clang-tidy-10 libeigen3-dev unzip
+set -e
+
+apt update
+apt install gpg
+# From https://apt.kitware.com/
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main' | tee /etc/apt/sources.list.d/kitware.list >/dev/null
+apt update
+apt install -y cmake --no-install-recommends``
+apt install -y clang clang-format-10 clang-tidy-10 libeigen3-dev unzip
 
 apt install -y python3-venv pip python3-gdal
