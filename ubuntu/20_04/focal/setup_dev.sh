760eb4e3b5f574b29258b56104f6953211726e4f
Sven Kautlenbach <sven.kautlenbach@cgi.com>
2023-01-02 13:40:31 +0200

    Renamed nvidia_base to cuda_base for Ubuntu 20.04 focal.

--- ubuntu/20_04/focal/setup_dev.sh
+++ ubuntu/20_04/focal/setup_dev.sh
@@ -8,7 +8,7 @@ apt install gpg
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
 echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main' | tee /etc/apt/sources.list.d/kitware.list >/dev/null
 apt update
-apt install -y cmake --no-install-recommends``
+apt install -y cmake --no-install-recommends
 apt install -y clang clang-format-10 clang-tidy-10 libeigen3-dev unzip
 
 apt install -y python3-venv pip python3-gdal
