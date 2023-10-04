bb84a8b7503f267a8c8075ed2b41f6aa37467d44
Sven Kautlenbach <sven.kautlenbach@cgi.com>
2023-10-04 12:30:37 +0300

    Modified RHEL 8 container installation for GPU to adaptively choose/install repo.

--- rhel/8/ootpa/install_container_toolkit.sh
+++ rhel/8/ootpa/install_container_toolkit.sh
@@ -5,12 +5,21 @@ set -e
 # Check if driver base is present
 nvidia-smi
 
-yum update
 yum install -y podman
 
-# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
-curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | \
-  sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+distro="rhel8"
+arch="x86_64"
+repository_name="cuda-$distro-$arch"
+
+if sudo dnf repolist | grep -q "$repository_name"; then
+    echo "Repository '$repository_name' is installed, installing nvidia-container-toolkit from there"
+    # Add your actions here if the repository is installed
+else
+    # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+    curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | \
+      sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+fi
+
 yum install -y nvidia-container-toolkit
 
 # For Podman, NVIDIA recommends using CDI for accessing NVIDIA devices in containers.
