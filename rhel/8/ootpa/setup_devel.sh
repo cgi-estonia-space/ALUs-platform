905917d2207a5ad1114eb551a74cb6f5253c603c
Sven Kautlenbach <sven.kautlenbach@cgi.com>
2023-10-12 11:02:04 +0300

    No need for Eigen in RHEL 8

--- rhel/8/ootpa/setup_devel.sh
+++ rhel/8/ootpa/setup_devel.sh
@@ -4,12 +4,3 @@ set -e
 
 dnf update -y
 dnf install -y boost1.78-devel cmake unzip git
-
-curl https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.zip --output /tmp/eigen-3.3.7.zip
-cd /tmp
-unzip eigen-3.3.7.zip
-cd eigen-3.3.7
-cmake -B build .
-cd build
-make install
-
